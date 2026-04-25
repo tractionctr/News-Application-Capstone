a896de584c4e1ac9c7cc068ee7117258c89c053e
Jaden <jadenmajavia@gmail.com>
2026-04-25 22:16:16 +0200

    Update settings.py

--- news_project/settings.py
+++ news_project/settings.py
@@ -128,5 +128,5 @@ EMAIL_USE_TLS = True
 # INTERNAL API
 INTERNAL_API_ENDPOINT = 'http://localhost:8000/api/approved/'
 
-# OPTIONAL (avoids annoying warnings)
+# (avoids annoying warnings)
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
