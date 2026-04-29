98e3bad71eef7fc14aa00e28f5763586f3ead51d
Jaden <jadenmajavia@gmail.com>
2026-04-29 20:56:49 +0200

    Update CSRF_TRUSTED_ORIGINS to remove HTTPS entries
    Removed HTTPS origins from CSRF_TRUSTED_ORIGINS.

--- news_project/settings.py
+++ news_project/settings.py
@@ -91,8 +91,6 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://localhost:8000",
-    "https://127.0.0.1:8000",
 ]
 
 # PASSWORD VALIDATION
