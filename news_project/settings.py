68bac3288096fb35e86f236bc614010d37e3da7b
Jaden <jadenmajavia@gmail.com>
2026-04-29 20:43:11 +0200

    Update settings.py

--- news_project/settings.py
+++ news_project/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://localhost:8000",
+    "https://127.0.0.1:8000",
+]
+
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
